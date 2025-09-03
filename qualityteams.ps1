682fe87336f4a4fb86a4a05f8fbb2b524a298b04
romesrmj <romesrmj@gmail.com>
2025-09-03 17:28:16 -0300

    Update qualityteams.ps1

--- qualityteams.ps1
+++ qualityteams.ps1
@@ -5,7 +5,7 @@
 # =======================
 
 # Define o destino para testes
-$target = "teams.microsoft.com"
+$target = "outlook.microsoft.com"
 
 Write-Host "`n=== AVALIAÇÃO DE CONEXÃO PARA MICROSOFT TEAMS ===" -ForegroundColor Cyan
 
